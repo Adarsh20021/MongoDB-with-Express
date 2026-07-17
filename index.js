9c912d19be261b4fc508125e6c46dd1da1822cb2
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-07-17 16:25:59 +0530

    Final Commit

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 main().then(()=>{console.log("Connection successful")})
 .catch(err => console.log(err));
@@ -19,10 +22,53 @@ async function main() {
 //Index Route
 app.get("/chats", async(req,res)=>{
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs",{chats});
+});
+
+//New Route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+//Create Route
+app.post("/chats",(req,res)=>{
+    let {from,message,to} = req.body;
+    let newChat = new Chat({
+        from: from,
+        message: message,
+        to: to,
+        created_at: new Date()
+    });
+    newChat.save().then((res)=>{console.log(res)}).catch((err)=>{console.log(err)});            //no await cauz its thennable (js knows its asynchronous )
+    res.redirect("/chats");
+});
+
+//Edit Route
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
+//Update Route
+app.put("/chats/:id",async (req,res)=>{
+    let {id} = req.params;
+    let {message:newMessage} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id,{message:newMessage},{runValidators:true, new:true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+//Delete Route
+app.delete("/chats/:id", async(req,res)=>{
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
 })
 
+//Root route
 app.get("/",(req,res)=>{
     res.send("Root is working");
 });

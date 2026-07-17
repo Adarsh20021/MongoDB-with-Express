d34fcb1a8ed683e15e8012d68f23716a8f19ed50
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-07-17 11:26:42 +0530

    Second Commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
 
 main().then(()=>{console.log("Connection successful")})
 .catch(err => console.log(err));

e6975e6970f4596ea5923d5b183fd163a3d1e8eb
Adarsh20021 <adarshkulkarni10@gmail.com>
2026-07-17 11:11:42 +0530

    Added Index.ejs

--- index.js
+++ index.js
@@ -15,14 +15,12 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 };
 
-let chat1 = new Chat({
-    from: "Neha",
-    to: "Priya",
-    message: "Send me your notes",
-    created_at: new Date(),
-});
-
-chat1.save().then((res)=>{console.log(res)});
+//Index Route
+app.get("/chats", async(req,res)=>{
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+})
 
 app.get("/",(req,res)=>{
     res.send("Root is working");
